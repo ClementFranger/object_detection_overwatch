a8a5aa2b93a782471608c64aef74368d15990026
Minifranger <clement.franger@gmail.com>
2022-07-30 20:30:54 +0200

    refacto

---
+++ test_tacticalvisor/test_recorder.py
@@ -0,0 +1,20 @@
+import unittest
+import logging
+
+from tacticalvisor.recorder import Recorder
+from workout.labelimg.data import Data
+
+logging.basicConfig(level=logging.INFO)
+
+
+class TestRecorder(unittest.TestCase):
+    dofus = r'C:\Users\Minifranger\Documents\python_scripts\workout\workout\dofus'
+    dofus_graph = r'C:\Users\Minifranger\Documents\python_scripts\workout\workout\dofus\models\ssd_mobilenet_v2_320x320_coco17_tpu-8\graph'
+
+    def setUp(self):
+        self.data = Data.factory(source=self.dofus)
+        # self.trained_model = TrainedModel.factory(source=self.dofus_graph)
+        self.recorder = Recorder.factory(title='Dofus', source=self.dofus_graph)
+
+    def test_stream(self):
+        self.recorder.stream()
